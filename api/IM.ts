75d75b78c71bbbd54830cc68f34cb567449fcc5d
niannings <niannings@163.com>
2019-09-13 00:39:38 +0800

    test

--- api/IM.ts
+++ api/IM.ts
@@ -4,6 +4,10 @@ import * as path from 'path';
 export default (req: any, res: any) => {
     fs.writeFile('/tmp/test.json', JSON.stringify(req.body), err => {
         console.log(err);
+
+        res.json({
+            data: require('/tmp/test.json')
+        })
     });
 
     res.sendStatus ? res.sendStatus(400) : res.send(400);
