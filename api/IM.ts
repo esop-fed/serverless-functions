c6c077217da7c550aca8f67953b340643e463b7c
niannings <niannings@163.com>
2019-09-13 00:12:48 +0800

    test

--- api/IM.ts
+++ api/IM.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 export default (req: any, res: any) => {
-    fs.writeFile(path.resolve(__dirname, '../../static/test.json'), JSON.stringify(req.body), err => {
+    fs.writeFile(path.resolve(__dirname, '../static/test.json'), JSON.stringify(req.body), err => {
         console.log(err);
     });
 
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import message from '../api/src/message';
+import IM from '../api/src/IM';
 
 const app = express();
 const port = process.env.PORT || 3040;
@@ -11,6 +12,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/message', message);
 app.post('/message', message);
+
+app.get('/IM', IM);
+app.post('/IM', IM);
 
 app.listen(Number(port), host, () => {
     console.log(`本地：http://${host}:${port}`);
